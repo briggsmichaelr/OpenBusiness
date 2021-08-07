9677a0bf07b652955fb8fcb613ecf0a6718fecfb
Joseph Briggs <josephpbriggs@Live.com>
2021-08-07 17:16:36 -0400

    I added the default folder as public, I added img and css folders to public and I added some text to the about page and changed color to gray

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 const {DB_USER, DB_PASS, DB_CLUSTER} = require('./secret.js')
 
