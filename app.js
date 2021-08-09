c68c5d34f90fa9d52c5bdba8705d79f536999eb1
Michael Briggs <briggsmichaelr@gmail.com>
2021-08-09 17:12:49 -0400

    added code to make simple sign up page that will check if a username exists and add it to the database if it does not.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+var bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.set('view engine', 'ejs');
 const {DB_USER, DB_PASS, DB_CLUSTER} = require('./secret.js')
 
 const { MongoClient } = require('mongodb');
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}.cipru.mongodb.net/OB?retryWrites=true&w=majority`; 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+
 async function main(){
-    const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}.cipru.mongodb.net/OB?retryWrites=true&w=majority`; 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     //const client = new MongoClient(uri);
  
     try {
@@ -40,12 +46,48 @@ main().catch(console.error);
 app.get('/', (req, res) => res.send('Hello from the Open World!'));
 app.get('/about/', (req,res) =>{
     res.render('about');
-})
+}) 
 app.get('/login/', (req,res) =>{
     res.render('login');
 })
+app.post('/login_request/', (req,res) =>{
+    let username = req.body.username;
+
+})
+app.get('/signup/',(req,res)=>{
+    res.render('signup');
+})
 app.get('/team/',function(req,res) {
     res.render("team.ejs");
-} )
+} );
+app.post('/signup_request/', async (req,res) =>{
+    console.log(req.body);
+    let username = req.body.username;
+    console.log(username);
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        let db = client.db("OB");
+        let users = db.collection("users");
+
+        let user = await users.find({username: username});
+        user = await user.toArray();
+        
+        console.log(user);
+        if(user.length === 0) {
+            await users.insertOne({username: username});
+            console.log(`user ${username} was just inserted!`);
+            res.redirect('/about');
+        } else {
+            console.log(`user ${username} already exists!`)
+            res.redirect('/login');
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        console.log('about to close');
+        await client.close();
+    }
+})
 
 app.listen(port, () => console.log(`Open Business app listening at http://localhost:${port}`))
