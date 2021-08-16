78b9c9218a34c71d9eaea8dfe44dc9a44b1dbb5c
Michael Briggs <briggsmichaelr@gmail.com>
2021-08-16 16:34:39 -0400

    created db_updateOne function and implemented the new functions throughout app.js

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}.cipru.mongodb.net/OB?retryWrites=true&w=majority`; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function db_find(collection,parameters) {
+async function db_find(collection,filter) {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
         let db = client.db("OB");
         let collection_temp = db.collection(collection);
-        let collection_filtered = await collection_temp.find(parameters);
+        let collection_filtered = await collection_temp.find(filter);
         collection_filtered = await collection_filtered.toArray();
         return collection_filtered;
     } catch (error) {
@@ -35,13 +35,13 @@ async function db_find(collection,parameters) {
         await client.close();
     }
 }
-async function db_insertOne(collection,parameters) {
+async function db_insertOne(collection,filter) {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
         let db = client.db("OB");
         let collection_temp = db.collection(collection);
-        let collection_filtered = await collection_temp.insertOne(parameters);
+        let collection_filtered = await collection_temp.insertOne(filter);
         return collection_filtered;
     } catch (error) {
         console.error(error);
@@ -49,21 +49,20 @@ async function db_insertOne(collection,parameters) {
         await client.close();
     }
 }
-//db_find("users",{username:"briggsmichaelr"})
-/*
-    let user = await db_find("users",{username:username})
-    if(user.length === 0) {            
-        await users.insertOne({username: username});
-        user = await users.find({username: username});
-        user = await user.toArray();
-        req.session.username = user[0].username;
-        console.log(`user ${username} was just inserted!`);
-        res.redirect('/about');
-    } else {
-        console.log(`user ${username} already exists!`)
-        res.redirect('/login');
+async function db_updateOne(collection,filter,updateFilter) {
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        let db = client.db("OB");
+        let collection_temp = db.collection(collection);
+        let collection_updated = await collection_temp.updateOne(filter,updateFilter);
+        return collection_updated;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
     }
-*/
+}
 
 app.get('/', (req, res) => res.render('home'));
 app.get('/about/', (req,res) =>{
@@ -91,35 +90,17 @@ app.get('/team/',function(req,res) {
     res.render("team.ejs");
 } );
 app.post('/signup_request/', async (req,res) =>{
-    console.log(req.body);
     let username = req.body.username_name;
-    console.log(username);
     let user = await db_find("users", {username: username});
-    //try {
-        // Connect to the MongoDB cluster
-       // await client.connect();
-       // let db = client.db("OB");
-        //let users = db.collection("users");
-
-        //let user = await users.find({username: username});
-        //user = await user.toArray();
-        
-        console.log(user);
-        if(user.length === 0) {            
-            await db_insertOne("users", {username: username});
-            user = await db_find("users", {username: username});
-            req.session.username = user[0].username;
-            console.log(`user ${username} was just inserted!`);
-            res.redirect('/about');
-        } else {
-            console.log(`user ${username} already exists!`)
-            res.redirect('/login');
-        }
-    //} catch (e) {
-      //  console.error(e);
-    //} finally {
-        //await client.close();
-    //}
+    if(user.length === 0) {            
+        await db_insertOne("users", {username: username});
+        req.session.username = username;
+        console.log(`user ${username} was just inserted!`);
+        res.redirect('/about');
+    } else {
+        console.log(`user ${username} already exists!`)
+        res.redirect('/login');
+    }
 })
 app.get('/create-organization/',(req,res)=>{
     res.render('create');
@@ -129,26 +110,14 @@ app.post('/create-organization-endpoint/',async (req,res)=>{
     //check if user is logged in
     if(req.session.username){
         let user = req.session.username;
-        try {
-            // Connect to the MongoDB cluster
-            await client.connect();
-            let db = client.db("OB");
-            let organizations = db.collection("organizations");
-            //Make sure organization does not currently exist
-            let Org = await organizations.find({name: Org_to_be_inserted, admin:user});
-            Org = await Org.toArray();
-            if(Org.length === 0) {
-                await organizations.insertOne({name: Org_to_be_inserted,admin:user});
-                console.log(`Org ${Org_to_be_inserted} was just inserted!`);
-                res.redirect(`/${user}/${Org_to_be_inserted}`);
-            } else {
-                console.log(`Org ${Org_to_be_inserted} already exists!`)
-                res.redirect('/create-organization');
-            }
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await client.close();
+        let Org = await db_find("organizations",{name:Org_to_be_inserted,admin:user});
+        if(Org.length === 0) {
+            await db_insertOne("organizations",{name: Org_to_be_inserted,admin:user});
+            console.log(`Org ${Org_to_be_inserted} was just inserted!`);
+            res.redirect(`/${user}/${Org_to_be_inserted}`);
+        } else {
+            console.log(`Org ${Org_to_be_inserted} already exists!`)
+            res.redirect('/create-organization');
         }
     } else {
         res.redirect('/login?logged_in=false&redirected_from=create-organization');
@@ -160,11 +129,7 @@ app.get('/:username/:organization',async (req,res)=>{
     let organization = req.params.organization;
     let user = req.session.username;
 
-    await client.connect();
-    let db = client.db("OB");
-    let organizations = db.collection("organizations");
-    let Org = await organizations.find({admin:user,name:organization});
-    Org = await Org.toArray();
+    let Org = await db_find("organizations",{name:organization,admin:admin});
     let content = Org[0].content;
     if(user==admin){
         res.render('organization',{admin:true,name:organization,content:content});
@@ -175,12 +140,7 @@ app.get('/:username/:organization',async (req,res)=>{
 app.get('/profile/' ,async (req,res)=>{
     if(req.session.username){
         let user = req.session.username;
-        await client.connect();
-        let db = client.db("OB");
-        let organizations = db.collection("organizations");
-        //Make sure organization does not currently exist
-        let Org = await organizations.find({admin:user});
-        Org = await Org.toArray();
+        let Org = await db_find("organizations",{admin:user});
         res.render('profile',{user:user,organizations:Org})
     } else {
         res.redirect('/login');
@@ -189,23 +149,11 @@ app.get('/profile/' ,async (req,res)=>{
 app.post('/create-folder/',async (req,res)=>{
     //define admin as person who clicked create folder
     let admin= req.session.username;
-    //connect to database
-    await client.connect();
-            let db = client.db("OB");
-            let organizations = db.collection("organizations");
-    //allow folders to be added to the data base of admin's organization
-    //allow admin to post folders
-    //
-    console.log('folder_to_be_inserted',req.body.folder_name);
-    console.log('folder_to_be_inserted',req.body.folder_name);
-
-
     let folder_to_be_inserted = req.body.folder_name;
     let org_name = req.body.organization_name
-    let Org = await organizations.updateOne({name:org_name, admin:admin}, {$set:{content:[{name:folder_to_be_inserted,type:'folder'}]}});
-    
-    await client.close();
-    res.send("okay")
+    //connect to database
+    await db_updateOne("organizations",{name:org_name, admin:admin}, {$set:{content:[{name:folder_to_be_inserted,type:'folder'}]}});
+    res.redirect(`/${admin}/${org_name}`);
 })
 
 app.listen(port, () => console.log(`Open Business app listening at http://localhost:${port}`))
